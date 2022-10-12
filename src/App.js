8667c3cba67606faa6d0df01bed2c7ff71e31bfd
chinmay3 <chinmaykhanna3@gmail.com>
2022-10-12 19:43:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
 
-function App() {
+import Logo from './images/logo.svg'
+import LaptopIcon from './images/icon-laptop.svg'
+import TwitterIcon from './images/icon-twitter.svg'
+import YoutubeIcon from './images/icon-youtube.svg'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/7fXlcwOnrFYAhVo2/scene.splinecode"
+      />
+      <Social>
+        <div />
+        <img src={TwitterIcon} alt="Twitter" />
+        <img src={YoutubeIcon} alt="YouTube" />
+      </Social>
+      <Content>
+      <Menu>
+          <li>
+            <img src={Logo} alt="title" />
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Download</a>
+          </li>
+          <li>
+            <a href="/">App</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+          <li>
+            <button>Get Started</button>
+          </li>
+        </Menu>
+        <h1>Collaborate with People</h1>
+        <p>Bring your team together and build your community by using our cross-platform app that lets you collaborate via chat, voice and by sharing and storing unlimited media files. A world of topics is waiting for you. Join the private beta.</p>
+        <button><img src={LaptopIcon} /> Download for Mac</button>
+      </Content>
+    </Wrapper>
   );
 }
+const Wrapper = styled.div`
+  font-family: "Spline Sans", sans-serif;
+  font-size: 16px;
 
-export default App;
+  position: relative;
+
+  .spline {
+    position: absolute;
+    margin-right: 100px;
+    top: -200px;
+    left: 30px;
+
+    @media (max-width: 1024px) {
+      transform: scale(0.8) translateX(200px);
+      transform-origin: top;
+    }
+    @media (max-width: 800px) {
+      transform: scale(0.7) translateX(600px);
+    }
+    @media (max-width: 600px) {
+      transform: scale(0.5) translateX(-100px);
+      right: auto;
+      left: 50%;
+      margin-left: -600px;
+    }
+    @media (max-width: 375px) {
+      transform: scale(0.45) translateX(-50px);
+    }
+  }
+`;
+
+const Content = styled.div`
+  position:absolute;
+  top:30px;
+  color:white;
+
+  display:flex;
+  flex-direction:column;
+  gap:80px;
+
+  h1{
+    font-family: 'Spline Sans Mono', monospace;
+    font-weight:bold;
+    font-size:70px;
+    margin:0;
+    max-width:500px;
+  }
+  p{
+    max-width:380px;
+    font-weight:normal;
+    line-height:150%;
+  }
+
+  button{
+    background: rgba(0,0,0,0.2);
+    border:0px;
+    font-size:16px;
+    padding:12px 30px;
+    border-radius:14px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    color:white;
+    max-width:280px;
+    backdrop-filter: blur(20px);
+    display:flex;
+    gap:12px;
+    justify-content:center;
+    align-items:center;
+    
+    transition:1s;
+
+    :hover{
+      border: 1px solid rgba(255, 255, 255, 0.8);
+      transform: translateY(-3px);
+    }
+  }
+
+  h1,p, button{
+    margin: 0 30px 0 100px;
+  }
+  `;
+
+  const Menu = styled.div`
+    display:flex;
+    gap:30px;
+    align-items:center;
+    margin:0 30px 0 100px;
+    padding: 0px;
+
+    li{
+      list-style:none;
+      margin:0;
+    }
+
+    a{
+      text-decoration: none;
+      color:white;
+      padding: 8px 20px;
+      border: 1px solid rgba(255,255,255,0)
+      transition:1s;
+      border-radius:14px;
+
+      :hover {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+    `
+
+const Social = styled.div`
+position: absolute;
+top: 150px;
+left: 30px;
+display: flex;
+flex-direction: column;
+gap: 30px;
+align-items: center;
+
+div {
+  width: 1px;
+  height: 500px;
+  background: linear-gradient(
+    180deg,
+    #08b6f9 0%,
+    #6c56ef 33.57%,
+    #1306dd 65.86%,
+    #aa0eb2 100%
+  );
+}
+`
